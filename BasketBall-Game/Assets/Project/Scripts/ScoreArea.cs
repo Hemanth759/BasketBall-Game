059b069a5f36daab6b48d7377b3ccd7f12906155
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-21 21:42:17 +0530

    Completed the tutorials

---
+++ BasketBall-Game/Assets/Project/Scripts/ScoreArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+
+    public GameObject ScoreAreaParticles;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        ScoreAreaParticles.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<BallParticle>() != null) 
+        {
+            ScoreAreaParticles.SetActive(true);
+        }
+    }
+}

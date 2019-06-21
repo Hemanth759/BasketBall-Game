6d2bfbfe90d120d79c8e70ace86e5ea8c6dd8726
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-21 18:17:17 +0530

    Implemented ball movement

---
+++ BasketBall-Game/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("game Object varialbles:")]
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    [Space]
+    [Header("Some public variables:")]
+    public float ballOffSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;        
+    }
+
+    void FixedUpdate()
+    {
+        ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballOffSet);
+    }
+}
